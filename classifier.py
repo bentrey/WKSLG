2c89c18d8f4f459063ec7704f4dfad67c2781379
Benjamin Trey <bentrey@gmail.com>
2021-09-11 20:42:26 -0700

    Add files via upload

--- classifier.py
+++ classifier.py
@@ -1,8 +1,18 @@
 import librosa
 import os
 import langdetect
+import urllib.request
+import time
 import pandas as pd
 import numpy as np
+from googlesearch import search
+from tqdm.notebook import tqdm
+#from sklearn.neural_network import MLPClassifier
+#from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import train_test_split
+
 
 def classifier(rewrite=False):
     #file name, lang, zero_crossing_rate, spectral_centroid,
@@ -19,18 +29,17 @@ def classifier(rewrite=False):
         'cs2m', 'cs3m', 'cs4m', 'cs5m', 'cs6m', 'cs7m', 'cs8m', 'cs9m', \
         'cs10m', 'cs11m', 'cs12m', 'cs1v', 'cs2v', 'cs3v', \
         'cs4v', 'cs5v', 'cs6v', 'cs7v', 'cs8v', 'cs9v', 'cs10v', \
-        'cs11v', 'cs12v', 'label']
+        'cs11v', 'cs12v', 'label','genre']
         df = pd.DataFrame(columns=columns)
     else:
-        df =  pd.read_csv('D:/songs.csv')
-    directory = 'D:/library/'
+        df =  pd.read_csv('songs.csv')
+    directory = 'library/'
     w = os.walk(directory)
     folders = next(w)[1]
-    for folder in folders:
+    for folder in tqdm(folders,desc="Folders",leave=False):
         songs = os.listdir(directory+folder)
-        for song_name in songs:
+        for song_name in tqdm(songs,desc="Songs",leave=False):
             if not song_name in list(df['name']):
-                print(song_name)
                 new_row = []
                 new_row.append(song_name)
                 new_row.append(langdetect.detect(song_name))
@@ -46,5 +55,184 @@ def classifier(rewrite=False):
                     new_row.append(-1)
                 else:
                     new_row.append(int(folder.split('_')[0]))
+                new_row.append('[]')
                 df.loc[df.index.max()+1] = new_row
-    df.to_csv('D:/songs.csv',index=False)
+                df.to_csv('songs.csv',index=False)    
+                
+def remove_missing():
+    w = os.walk('library/')
+    files = []
+    new_files = next(w,False)
+    while new_files:
+        files.extend(new_files[2])
+        new_files = next(w,False)
+    df = pd.read_csv('songs.csv')
+    df = df[df['name'].isin(files)]
+    df.to_csv('songs.csv',index=False)
+    
+def get_pickles():
+    pass
+
+def get_genres(rewrite=False,google=False):
+    df = pd.read_csv('songs.csv')
+    artists = list(df['name'])
+    empties = df['genre'] == '[]'
+    for n in tqdm(range(len(artists)),desc="songs",leave=False):
+        try:
+            if '-' in artists[n]:
+                artists[n] = ' '.join(artists[n].split('-')[:-1])
+            if artists[n][-1] == ' ':
+                artists[n] = artists[n][:-1]
+            if empties[n] or rewrite:
+                url = 'https://www.last.fm/music/'+artists[n].replace(' ','+')
+                if google:
+                    urls = list(search(artists[n]+' last.fm'))
+                    url = urls[0]
+                    time.sleep(58+30*np.random.rand())
+                    for temp_url in urls:
+                        if 'last.fm' in temp_url and not 'last.fm' in url:
+                            url = temp_url
+                file = urllib.request.urlopen(url).read().decode("utf-8")
+                if 'class="catalogue-tags' in file:
+                    tags = file.split('class="catalogue-tags "')[1].split("</ul>")[0].replace(' ','')
+                    tags_list = tags.split("/tag/")[1:]
+                if len(tags_list)>1:
+                    for m in range(len(tags_list)):
+                        tags_list[m] = tags_list[m].split('"')[0]
+                df.iat[n,70] = str(tags_list)
+                df.to_csv('songs.csv',index=False) 
+        except Exception as e:
+            print(artists[n])
+            print(e)
+
+def get_songs_for_genre(label,number_of_songs,alpha=0.05):
+    #opening songs.cvs, giving label to songs in two locations
+    df = pd.read_csv('safesongs.csv')
+    songs = []
+    for folder in os.listdir('library'):
+        if str(label) == folder.split('_')[0]:
+            songs = os.listdir('library/'+folder)
+            df_duplicates = df[df['name'].isin(songs)].copy()
+            df_duplicates['label'] = label
+            df = pd.concat([df,df_duplicates]).drop_duplicates().reset_index(drop=True)
+    #finding genres in training data
+    folder_df = df[df['label']>=0]
+    genre_labels = []
+    for index,row in folder_df.iterrows():
+        genre_list = row['genre'][2:-2].split("', '")
+        for genre in genre_list:
+            if not genre in genre_labels:
+                genre_labels.append(genre)
+    #vectorizing data
+    df = df[['name','label','genre']]
+    df['genre'] = df.genre.apply(lambda x: x[2:-2].split("', '"))
+    mlb = MultiLabelBinarizer(sparse_output=True)
+    df = df.join(
+        pd.DataFrame.sparse.from_spmatrix(
+        mlb.fit_transform(df.pop('genre')),
+        index=df.index,
+        columns=mlb.classes_))
+    #training
+    df['bool_label'] = df.label.apply(lambda x: x==label)
+    drops = []
+    for column in df.columns:
+        if not column in genre_labels+['label','bool_label','name']:
+            drops.append(column)
+    df = df.drop(drops,axis=1)
+    df_training = df[df['label']>=0].drop('label',axis=1)
+    df_rotation = df[df['label']<0].drop('label',axis=1)
+    y = df_training['bool_label']
+    X = df_training.drop(['bool_label','name'],axis=1)
+    keep_going = True
+    while keep_going:
+        try:
+            lr = LogisticRegression()
+            lr.fit(X,y)
+            keep_going = False
+        except:
+            pass
+    probabilities = lr.predict_proba(df_rotation.drop(['name','bool_label'],1))
+    maxes = probabilities[:,1].copy()
+    maxes.sort()
+    df_rotation = df_rotation[probabilities[:,1] >= max([maxes[-number_of_songs],alpha])]
+    #df_rotation = df_rotation[probabilities[:,1] > maxes[-number_of_songs]]
+    return list(df_rotation['name'])
+
+def get_songs_for_genre_kmeans(label):
+    #opening songs.cvs, giving label to songs in two locations
+    df = pd.read_csv('safesongs.csv')
+    songs = []
+    for folder in os.listdir('library'):
+        if str(label) == folder.split('_')[0]:
+            songs = os.listdir('library/'+folder)
+            df_duplicates = df[df['name'].isin(songs)].copy()
+            df_duplicates['label'] = label
+            df = pd.concat([df,df_duplicates]).drop_duplicates().reset_index(drop=True)
+    #finding genres in training data
+    folder_df = df[df['label']==-1]
+    genre_labels = []
+    for index,row in folder_df.iterrows():
+        genre_list = row['genre'][2:-2].split("', '")
+        for genre in genre_list:
+            if not genre in genre_labels:
+                genre_labels.append(genre)
+    #getting final columns
+    df_training = df[df['label']==-1]
+    df_training = df_training[['name','label','genre']]
+    df_training['genre'] = df_training.genre.apply(lambda x: x[2:-2].split("', '"))
+    mlb = MultiLabelBinarizer(sparse_output=True)
+    df_training = df_training.join(
+        pd.DataFrame.sparse.from_spmatrix(
+        mlb.fit_transform(df_training.pop('genre')),
+        index=df_training.index,
+        columns=mlb.classes_))
+    #vectorizing data
+    df = df[['name','label','genre']]
+    df['genre'] = df.genre.apply(lambda x: x[2:-2].split("', '"))
+    mlb = MultiLabelBinarizer(sparse_output=True)
+    df = df.join(
+        pd.DataFrame.sparse.from_spmatrix(
+        mlb.fit_transform(df.pop('genre')),
+        index=df.index,
+        columns=mlb.classes_))
+    df = df[df.columns.intersection(list(df_training.columns))]
+    #training
+    y = df['name']
+    X = df.drop(['name','label'],axis=1)
+    keep_going = True
+    while keep_going:
+        try:
+            kmeans = KMeans(n_clusters=20, random_state=0).fit(X)
+            keep_going = False
+        except:
+            pass
+    df['kmeans'] = kmeans.labels_
+    genre_songs = get_songs_for_genre(label,500)
+    label_value = df[df['name'].isin(genre_songs)]['kmeans'].value_counts().idxmax()
+    df = df[df['kmeans']==label_value]
+    return list(df['name'])
+
+def get_songs_for_genre_2(label,number_of_songs):
+    df = pd.read_csv('safesongs.csv')
+    for folder in os.listdir('D:/library'):
+        if str(label) == folder.split('_')[0]:
+            print(label)
+            songs = os.listdir('D:/library/'+folder)
+            df_duplicates = df[df['name'].isin(songs)].copy()
+            df_duplicates['label'] = label
+            df = pd.concat([df,df_duplicates]).drop_duplicates().reset_index(drop=True)
+    df_training = df[df['label'] == label]
+    print(df_training['label'].value_counts())
+    genres_dictionary = {}
+    for index,row in df_training.iterrows():
+        genres = row['genre'][2:-2].split("', '")
+        for genre in genres:
+            genres_dictionary[genre] = genres_dictionary.get(genre,0) + 1
+    df_rotation = df[df['label'] < 0]
+    df_rotation = df_rotation[['name','genre']]
+    df_rotation['score'] \
+        = df_rotation.genre.apply(lambda x: sum([genres_dictionary.get(y,0) for y in x[2:-2].split("', '")]))
+    scores = list(df_rotation['score'])
+    scores.sort()
+    df_rotation = df_rotation[df_rotation['score'] > scores[-number_of_songs]]
+    return list(df_rotation['name'])
--- downloader.py
+++ downloader.py
@@ -4,31 +4,55 @@ import spotipy
 import os
 import random
 import youtube_dl
+import credentials
+import time
 import pandas as pd
 import numpy as np
-import credentials
 
 path = 'library'
 
-def get_file(artist, song, dir="/library"):
+def get_file(artist, song, dir="library", max_views=2*10**4):
     index = 0
     term = artist + ' ' + song
-    urls = google_search(term)
+    #urls = google_search(term)
     ydl_opts = {'format': 'bestaudio/best', 'postprocessors': [{'key': 'FFmpegExtractAudio',\
         'preferredcodec': 'mp3', 'preferredquality': '192',}],'noplaylist':True, \
-        'outtmpl': path + '/'+artist+' - '+song+'.%(ext)s',}
+        'outtmpl': path + '/'+artist+' - '+song+'.%(ext)s', 'quite':True,}
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        while index < len(urls):
-            url = urls[index]
-            if 'youtube.com' in url:
-                entry = ydl.extract_info(url, download=False)
-                title = entry['title']
-                if title_tester(title, term):
-                    entries = ydl.extract_info(url)
-                    index = len(urls)
-            index += 1
+        entries = ydl.extract_info(f"ytsearch:{artist} {song}", download=False)
+        total_views = 0
+        for n in range(len(entries['entries'])):
+            total_views += entries['entries'][n]['view_count']
+        url = entries['entries'][0]['webpage_url']
+        if total_views<max_views:
+            ydl.extract_info(url)
+        else:
+            print('Too many views')
+
+def get_views(artist, song, dir="library"):
+    try:
+        index = 0
+        term = artist + ' ' + song
+        #urls = google_search(term)
+        ydl_opts = {'quite':True, 'no_warnings':True, 'format': 'bestaudio/best', 'postprocessors': \
+                   [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality'\
+                   : '192',}],'noplaylist':True, 'outtmpl': path + '/'+artist+' - '+song+'.%(ext)s',\
+                   'ignoreerrors':True}
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            entries = ydl.extract_info(f"ytsearch:{artist} {song}", download=False)
+            song_total_views = 0
+            for n in range(len(entries['entries'])):
+                song_total_views += entries['entries'][n]['view_count']
+            entries = ydl.extract_info(f"ytsearch:{artist}", download=False)
+            artist_total_views = 0
+            for n in range(len(entries['entries'])):
+                artist_total_views += entries['entries'][n]['view_count']
+        return song_total_views, artist_total_views
+    except:
+        return 10**9, 10**9
 
 def google_search(terms):
+    time.sleep(58+30*np.random.rand())
     return list(search(terms, stop=10))
 
 def title_tester(title, term):
@@ -44,17 +68,18 @@ def title_tester(title, term):
         okay = False
     return okay
 
-def wfmu_downloader(includes=None):
+def wfmu_downloader(times,includes=None, max_views=2*10**4):
     wfmu_playlists = os.listdir('wfmu_html')
-    for n in range(10):
+    for n in range(times):
         file_name = random.choice(wfmu_playlists)
         print(file_name)
-        file = open('wfmu_html/'+file_name).read()
+        file = open('wfmu_html/'+file_name,errors='replace').read()
         if includes != None:
             while not includes in file:
                 file_name = random.choice(wfmu_playlists)
                 print(file_name)
                 file = open('wfmu_html/'+file_name).read()
+        df = ''
         tables = file.split('<table')
         for n in range(len(tables)):
             tables[n] = '<table'+tables[n].split('</tables>')[0]+'</table>'
@@ -65,7 +90,9 @@ def wfmu_downloader(includes=None):
                 columns = [str(col).lower() for col in list(test_df.columns)]
                 if 'artist' in columns:
                     df = test_df
-        files = np.array(os.listdir('library'))
+                elif 'the stooge' in columns:
+                    df = test_df
+        files = np.array(os.listdir('library/rotation'))
         if len(df)>0:
             df = df.fillna(method='ffill')
             for n in range(df.shape[0]):
@@ -74,22 +101,80 @@ def wfmu_downloader(includes=None):
                     artist = row[df.columns[0]]
                     song = row[df.columns[1]]
                     print(artist,song)
-                    artist_tracks = np.flatnonzero(np.core.defchararray.find(files,artist)!=-1)
-                    song_tracks = np.flatnonzero(np.core.defchararray.find(files,song)!=-1)
-                    same_song_locations = np.intersect1d(artist_tracks, song_tracks)
-                    if len(same_song_locations) == 0:
-                        get_file(artist, song)
+                    #artist_tracks = np.flatnonzero(np.core.defchararray.find(files,artist)!=-1)
+                    #song_tracks = np.flatnonzero(np.core.defchararray.find(files,song)!=-1)
+                    #same_song_locations = np.intersect1d(artist_tracks, song_tracks)
+                    #if len(same_song_locations) == 0 and (len(artist)+len(song))<100:
+                    get_file(artist, song, max_views=max_views)
                 except:
                     print('get_file() failed')
 
-def spotify_playlist_downloader(playlist_id):
+def spotify_playlist_downloader(playlist_id, max_views=2*10**4):
+    n=0
     cid = credentials.cid
     secret = credentials.secret
     client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
     spotify = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
-    results = spotify.playlist('2OOarnLNBNgkHMbT3rgUCJ')
-    for item in results['tracks']['items']:
-        artist = (item['track']['artists'][0]['name'])
-        track = (item['track']['name'])
+    results = spotify.playlist(playlist_id, fields="tracks,next")
+    downloads = []
+    tracks = results['tracks']
+    while tracks:
+        for item in tracks['items']:
+            artist = (item['track']['artists'][0]['name'])
+            track = (item['track']['name'])
+            downloads.append((artist,track))
+        tracks = spotify.next(tracks)
+    for download in downloads:
+        artist, track = download
+        try:
+            print(artist,track,n)
+            n += 1
+            get_file(artist,track,max_views=max_views)
+        except:
+            print('get_file failed')
+
+def spotify_album_downloader(album_id, max_views=2*10**4):
+    cid = credentials.cid
+    secret = credentials.secret
+    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+    spotify = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+    results = spotify.album(album_id)
+    for n in range(len(results['tracks']['items'])):
+        artist = results['artists'][0]['name']
+        track = results['tracks']['items'][n]['name']
         print(artist,track)
-        get_file(artist,track)
+        try:
+            get_file(artist,track,max_views=max_views) 
+        except:
+            print('get_file failed')
+
+def library_downloader(trials):
+    w = os.walk('/library/')
+    files = os.listdir('/library/rotation/')
+    cid = credentials.cid
+    secret = credentials.secret
+    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+    spotify = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+    for trial in range(trials):
+        try:
+            keep_going = True
+            index = [0]
+            while keep_going:
+                artist = '-'.join(random.choice(files).split('-')[:-1])
+                if sum([int(artist in name) for name in files])<5:
+                    items = spotify.search(artist,type='playlist')['playlists']['items']
+                    if len(items)>0:
+                        for n in range(len(items)):
+                            if items[n]['name'].lower() == 'this is '+artist.lower():
+                                keep_going = False
+                                index = n
+                        for n in range(len(items)):
+                            if items[n]['name'].lower() == artist.lower()+' radio':
+                                keep_going = False
+                                index = n
+            #id = items[random.randint(0,len(items))]['id']
+            id = items[n]['id']
+            spotify_playlist_downloader(id)
+        except:
+            time.sleep(100)
+
